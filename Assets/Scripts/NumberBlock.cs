0ce3e16da4f24d9eba45ca6dfbe2bbc949915d90
Fynjy <antonruckov@gmail.com>
2023-10-11 13:31:33 +0300

    Add and settings up TextMesjPro plugin.

--- Assets/Scripts/NumberBlock.cs
+++ Assets/Scripts/NumberBlock.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class NumberBlock : MonoBehaviour
 {
     public Rigidbody NumberBlockBody;
+
+    private List<TextMeshProUGUI> _blockCountNumbersTexts;
 }
