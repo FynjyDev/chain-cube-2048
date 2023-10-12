d7fcb912dc85fa3dca4e27cad3715d15a4730608
Fynjy <antonruckov@gmail.com>
2023-10-12 10:13:52 +0300

    Block spawn calculations have been done more correctly.

--- Assets/Scripts/Data/BlocksDataManager.cs
+++ Assets/Scripts/Data/BlocksDataManager.cs
@@ -7,6 +7,10 @@ public class BlocksDataManager : ScriptableObject
     public enum BlockTypes {number, rainbow , bomb  }
 
     public NumberBlock _blockPrefab;
+
+    [SerializeField] private int _minStartIndex = 1;
+    [SerializeField] private int _interval = 3;
+
     [SerializeField] private List<BlockData> _blockDatas;
 
     public List<BlockData> GetAllBlockDatas()
@@ -31,7 +35,14 @@ public class BlocksDataManager : ScriptableObject
     public BlockData GetDataByPlayerProgress(List<NumberBlock> currentBlocks)
     {
         BlockData maxBlockData = GetMaxBlockData(currentBlocks);
-        return _blockDatas[Random.Range(0, _blockDatas.IndexOf(maxBlockData))];
+
+        int maxExlusive = _minStartIndex;
+        int maxBlockDataIndex = _blockDatas.IndexOf(maxBlockData);
+
+        if (maxBlockDataIndex > _minStartIndex + _interval) 
+            maxExlusive = maxBlockDataIndex - _interval;
+
+        return _blockDatas[Random.Range(0, maxExlusive)];
     }
 
     public BlockData GetMaxBlockData(List<NumberBlock> currentBlocks)
@@ -39,7 +50,7 @@ public class BlocksDataManager : ScriptableObject
         List<BlockData> blockDatas = new List<BlockData>();
         currentBlocks.ForEach(x => blockDatas.Add(x.GetBlockData()));
 
-         BlockData maxBlockData = currentBlocks.Count <= 0 ? _blockDatas[0] : blockDatas[0];
+         BlockData maxBlockData = currentBlocks.Count <= 0 ? _blockDatas[1] : blockDatas[0];
 
         foreach (BlockData b in blockDatas)
             if (b.BlockCount > maxBlockData.BlockCount)
